50d320c63c866565b34acf47d67dec59a6db4e88
gaurav <gauravpusadkar931@gmail.com>
2025-03-03 23:16:25 +0530

    It was add sum of number

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python-learning-
+print('HOW'S YOUR DAY GOING')
---
+++ data type 2.py
@@ -0,0 +1,6 @@
+thistuple = ("apple","banana","mango")
+print(thistuple)
+thistuple = ("apple","banana","mango")
+print(len(thistuple))
+mytuple = ("apple","banana","mango")
+print(type(mytuple))
